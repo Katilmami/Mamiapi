a4d35f0c559ed58f0c1dcbbe3e6bc36a3c806545
Katilmami <akbalmuhammed668@gmail.com>
2024-01-13 20:42:02 +0300

    Update app.py

--- app.py
+++ app.py
@@ -55,4 +55,4 @@ def mamimod():
     return render_template('mamimod.html', current_content=current_content)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)
